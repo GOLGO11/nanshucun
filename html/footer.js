29dea04ee807af004fc862a10675d3746bc2350f
GOLGO11 <873200646@qq.com>
2024-04-10 17:30:59 +0800

    增加底部备案号

--- html/footer.js
+++ html/footer.js
@@ -2,7 +2,7 @@ document.writeln("<footer>");
 document.writeln("            <div class=\'footer-content\' align=\'center\'>");
 document.writeln("                <p>版权所有 &copy; 2024 天元街道楠树村. All rights reserved.</p>");
 document.writeln("                <p>地址：四川省德阳市旌阳区天元街道楠树村村委会</p>");
-document.writeln("                <p><a href=\'https://beian.miit.gov.cn/\' target=\'_blank\'>蜀ICP备2024069277号</a></p>");
+document.writeln("                <p><a href=\'https://beian.miit.gov.cn/\' target=\'_blank\'>蜀ICP备2024069277号-1</a></p>");
 document.writeln("                <a href=\'联系我们.html\'>联系我们</a>");
 document.writeln("            </div>");
 document.writeln("        </footer>");
